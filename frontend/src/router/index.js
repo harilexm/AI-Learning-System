f399dfa360afdc62d1dbd0a6021551cf0ae6908f
harilexm <umer.abdullah9891@gmail.com>
2025-11-08 13:41:26 +0500

    Code enhancement

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,9 @@ import RegisterView from '../views/RegisterView.vue'
 import { useAuthStore } from '@/stores/auth'
 import axios from 'axios';
 
+const apiClient = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -178,5 +181,18 @@ router.beforeEach(async (to, from, next) => {
   // Otherwise, allow navigation
   next();
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default router
